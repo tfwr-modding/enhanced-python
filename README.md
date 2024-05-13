0dfe9073fd58acc5a435241b5d34ae31c2c966f9
Angelos Bouklis <me@arjix.dev>
2024-05-13 17:48:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿## mod template
+
+A starter mod template for [TheFarmerWasReplaced](https://store.steampowered.com/app/2060160/The_Farmer_Was_Replaced).
+
+## 🚀 Getting Started
+1. Have BepInEx installed
+2. Clone the repository
+3. Run `dotnet build`
+4. Copy the `ExampleMod.dll` to the `Plugins` folder under `BepInEx/plugins`
+5. Run the game
+
+## 📚 Resources
+- [TheFarmerWasReplaced](https://store.steampowered.com/app/2060160/The_Farmer_Was_Replaced)
+- [BepInEx](https://github.com/BepInEx/BepInEx)
---
+++ src/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ExampleMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance { get; private set; } = null!;
+    public static ManualLogSource Log { get; private set; } = null!;
+    
+    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+    
+    private void Awake()
+    {
+        Instance = this;
+        Log = Logger;
+        
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        harmony.PatchAll();
+    }
+}
